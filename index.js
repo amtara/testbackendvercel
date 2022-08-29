ede94ecaf018a0ac72c4189b6047e602c5a123f5
maka123 <maka.toure@crea-inseec.com>
2022-08-29 15:37:31 +0200

    add change users

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.listen(5001, () => {
   console.log("backend server run on port ");
 });
 
-// app.get("/", (req, res) => {
-//   res.send("<h1>hello word</h1>");
-// });
+app.get("/", (req, res) => {
+  res.send("<h1>hello word</h1>");
+});
