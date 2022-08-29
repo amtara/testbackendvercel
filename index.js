2b6ccf755c761cc96f8fd7fe8be6d997f3a57452
maka123 <maka.toure@crea-inseec.com>
2022-08-29 15:28:32 +0200

    add change users

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const user = require("./api/user");
+const users = require("./api/user");
 const app = express();
 app.use(express.json({ extended: false }));
-
+app.use("/api/users", users);
 app.listen(5001, () => {
   console.log("backend server run on port ");
 });
 
-app.get("/", (req, res) => {
-  res.send("<h1>hello word</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>hello word</h1>");
+// });
